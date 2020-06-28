d0fc81216569f903f13eaa64fc8fcb03c0f73249
Zebra <zebroff1986@gmail.com>
2020-06-28 22:11:27 +0300

    second day

--- js/main.js
+++ js/main.js
@@ -1,12 +1,55 @@
 $(document).ready(function () {
-    $(".lookMap").hover(function (params) {
-        $("#map").fadeIn();
-        $("#map").mouseout(function (params) {
-            $(this).fadeOut(2000)
-        })
+    $(".mapContainer").hover(function () {
+        $(".map").stop().fadeIn(200);
+    });
+
+    $(".mapContainer").mouseleave(function () {
+        $(".map").stop().fadeOut(500);
     })
-    $(".hint").click(function (params) {
+
+    $(".hint").hover(function () {
         $(".hintMessage").toggleClass("hintMessageActive");
    });
+
+    $(".openPopup").click(function () {
+        $(".generalWrap").css("filter", "blur(5px)");
+        $(".popupWindow").fadeIn(500);
+    });
+
+    $("#popupForm").submit(function (event) {
+        event.preventDefault();
+    })
+
+    $(".close1").click(function () {
+        $(".popupWindow").fadeOut(200);
+        $(".generalWrap").css("filter", "none");
+    });
+
+    $("#popupForm").submit(function (event) {
+        event.preventDefault();
+        $(".popupWindow").fadeOut(100);
+        $(".recievedWindow").fadeIn(500);
+        $(".close2").click(function () {
+            $(".recievedWindow").fadeOut(500);
+            $(".generalWrap").css("filter", "none");
+        })   
+    })
+
+    $(".burger").click(function () {
+        $(this).toggleClass("burger_active");
+       $(".mainMenu").toggleClass("active");
+   });
+
+    var image = document.getElementsByClassName('thumbnail');
+    new simpleParallax(image, {
+        scale: 3,
+        overflow: true
+    });
+
+    var image2 = document.getElementsByClassName('thumbnail2');
+    new simpleParallax(image2, {
+        scale: 3,
+        overflow: true
+    }); 
 
 })
